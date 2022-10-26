72d914594de03b794e372406c386706890fcbd4b
yunicoin <90954972+yunicoin@users.noreply.github.com>
2022-10-26 13:28:32 +0300

    Add files via upload

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -6,4 +6,6 @@ from django.urls import include, path
 urlpatterns = [
     path('bookshell/', include('bookshell.urls')),
     path('admin/', admin.site.urls),
-]
+    path('mirror/', include('django.contrib.auth.urls'), name='login'),
+    path('mirror/', include('userss.urls'))
+],
